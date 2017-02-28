a05e92005a7741c56498b875a9e45f9d2e8749b9
Gloria Friesen and Mark Fisher <mark@>
2017-02-28 13:48:27 -0800

    add login button to second form that triggers a div full of other option buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,30 @@
 
 // Front End
 $(function(){
-
+  $("#userAccountForm").submit(function(){
+    event.preventDefault();
+    var usrName = $("#nameInput").val();
+    var usrPhone = $("#phoneInput").val();
+    var usrStreet = $("#streetInput").val();
+    var usrCity = $("#cityInput").val();
+    var usrState = $("#stateDropdown").val();
+    console.log(usrName, usrPhone,usrStreet,usrCity,usrState);
+    // potentially make function
+  });
+  $("#bankAccountForm").submit(function(){
+    event.preventDefault();
+    $("#optionsDiv").show();
+    var usrAccountName = $("#accountName").val();
+    var usrInitialDeposit = $("#initialDeposit").val();
+    $("#withdrawBtn").click(function(){
+      console.log("You want to withdraw");
+    });
+    $("#depositBtn").click(function(){
+      console.log("You want to deposit");
+    });
+    $("#balanceBtn").click(function(){
+      console.log("You just want to know things");
+    });
+    console.log(usrAccountName,usrInitialDeposit);
+  });
 });
